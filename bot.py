191fa9ce7488d0408dda78b9906b5f329be39361
sergeyparshi <tannkk321@gmail.com>
2024-05-12 12:58:04 +0700

    сделал команды и сайт

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from preload.config import TOKEN
 
-bot = Bot(token="7096491396:AAFSRQHELSCCRfTKCPQUarutShBIYbQyQLk")
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привет! Я бот Yarik и я умею делать видео в кружок")
+    await message.answer("Привет! Я бот Yarik , отправь видео")
 
 
 async def main():
@@ -16,5 +17,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
