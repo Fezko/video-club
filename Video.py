43e315449a6b36b05ec3df16e05e0a19a6b24ae2
PYTHON <tannkk321@gmail.com>
2024-05-18 16:59:53 +0700

    сделал бота с приветствием

--- Video.py
+++ Video.py
@@ -1,7 +1,8 @@
 import asyncio
-import F
+from aiogram import F
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.types import ContentType
 
 bot = Bot(token="7096491396:AAFSRQHELSCCRfTKCPQUarutShBIYbQyQLk")
 dp = Dispatcher()
@@ -9,6 +10,8 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Привет! Я бот Yarik и я умею делать видео в кружок,отправь видео!")
+
+@dp.message(F.content_type==ContentType.VIDEO)
 
 
 async def main():
@@ -19,10 +22,3 @@ if __name__ == "__main__":
     asyncio.run(main())
 
 
-@dp.message(content_types=['video'])
-def send_text(message):
-    file_info = bot.get_file(message.video.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open('video.mp4', 'wb') as video:
-        video.write(downloaded_file)
-    bot.send_video_note(message.chat.id, open('video.mp4', 'rb'))
