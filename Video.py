6599faaf055cbfdaf30ce336663875ab16da4ca1
PYTHON <tannkk321@gmail.com>
2024-05-25 16:48:43 +0700

    сделал бота с приветствием

--- Video.py
+++ Video.py
@@ -3,6 +3,7 @@ from aiogram import F
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import ContentType
+from aiogram.types import Video
 
 bot = Bot(token="7096491396:AAFSRQHELSCCRfTKCPQUarutShBIYbQyQLk")
 dp = Dispatcher()
@@ -12,6 +13,13 @@ async def cmd_start(message: types.Message):
     await message.answer("Привет! Я бот Yarik и я умею делать видео в кружок,отправь видео!")
 
 @dp.message(F.content_type==ContentType.VIDEO)
+async def video_id(message):
+  document_id = message.video.file_id
+  file_info = await bot.get_file(document_id)
+  print(document_id)
+  # video = Video(file_unique_id=document_id, width=300, height=300)
+  await bot.send_message(message.chat.id, document_id)
+  await bot.send_video_note(message.chat.id, document_id, )
 
 
 async def main():
