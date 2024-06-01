293f64bf1f384982e9a820513a074509bc722298
sergeyparshi <tannkk321@gmail.com>
2024-06-01 16:51:58 +0700

    оправляется кружок

--- Video.py
+++ Video.py
@@ -4,6 +4,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import ContentType
 from aiogram.types import Video
+import moviepy.editor as mpy
+from aiogram.types import FSInputFile
 
 bot = Bot(token="7096491396:AAFSRQHELSCCRfTKCPQUarutShBIYbQyQLk")
 dp = Dispatcher()
@@ -14,12 +16,15 @@ async def cmd_start(message: types.Message):
 
 @dp.message(F.content_type==ContentType.VIDEO)
 async def video_id(message):
-  document_id = message.video.file_id
-  file_info = await bot.get_file(document_id)
-  print(document_id)
-  # video = Video(file_unique_id=document_id, width=300, height=300)
-  await bot.send_message(message.chat.id, document_id)
-  await bot.send_video_note(message.chat.id, document_id, )
+    file_id = message.video.file_id  # Get file id
+    file = await bot.get_file(file_id)  # Get file path
+    await bot.download_file(file.file_path, "video.mp4")
+    video = mpy.VideoFileClip("video.mp4")
+    video = video.resize((360, 360))
+    video.write_videofile('video-cropped.mp4')
+    video = FSInputFile('video-cropped.mp4')
+
+    await bot.send_video_note(message.chat.id, video)
 
 
 async def main():
