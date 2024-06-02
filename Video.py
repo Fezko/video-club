f110ccc63e581fadcd6a499dfe12310e806f7b81
sergeyparshi <tannkk321@gmail.com>
2024-06-02 12:57:35 +0700

    зделал requirements.txt

--- Video.py
+++ Video.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import ContentType
 from aiogram.types import Video
-
+from moviepy.video.fx.all import crop
 import moviepy.editor as mpy
 from aiogram.types import FSInputFile
 
@@ -13,7 +13,7 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привет! Я бот Yarik и я умею делать видео в кружок,отправь видео!")
+    await message.answer("Привет! Я бот Yarik и я могу делать видео в кружок,отправь   мне видео!")
 
 @dp.message(F.content_type==ContentType.VIDEO)
 async def video_id(message):
@@ -22,7 +22,13 @@ async def video_id(message):
     file = await bot.get_file(file_id)  # Get file path
     await bot.download_file(file.file_path, "video.mp4")
     video = mpy.VideoFileClip("video.mp4")
-    video = video.resize((360, 360))
+    w, h = video.size
+    if w >= h:
+        cropped_clip = crop(video, width=h, height=h, x_center=w / 2, y_center=h / 2)
+    else:
+        cropped_clip = crop(video, width=w, height=w, x_center=w / 2, y_center=h / 2)
+
+    video = cropped_clip.resize((360, 360))
     video.write_videofile('video-cropped.mp4')
     video = FSInputFile('video-cropped.mp4')
 
